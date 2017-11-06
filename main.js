a6df2429061538e25ee74389f1a7407cfd26380c
tfermoso <tomas.fermoso@gmail.com>
2017-11-06 19:34:06 +0100

    paso 1

---
+++ main.js
@@ -0,0 +1,16 @@
+/**
+ * @fileOverview Este archivo contiene ejemplos de funciones con for
+ * @author Tomás Fermoso
+ * @version 1.0
+ */
+
+ /**
+  * @description sumaArray Suma los valores de un array
+  * @param {Array} numeros array de números
+  * @return {Number}
+  */
+  function sumaArray(numeros){
+    let suma=0;
+
+    return suma;
+  }
